1c723ba6a7af69f77c9aabdbeee567062f85ac00
MD. TASRIK <118077308+Md-Tasrik@users.noreply.github.com>
2026-02-01 12:16:02 +0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>CSTE CLUB Voting System</b>
+      </div>
+      <strong> &copy; Developed By <a href="#">Rupa & Tasrik</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
